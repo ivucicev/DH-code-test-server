90318badbb67e3dd374b65a6ba6342e6d1bd020e
ivucicev <ivucicev@gmail.com>
2019-09-24 15:05:14 +0200

    Finalizing encoder, created encoder, added assertion tests, created route for encoding, validated sequence

---
+++ modules/encoder.ts
@@ -0,0 +1,20 @@
+export class Encoder {
+    public static encode(sequence: string): string | null {
+        if (!/^[a-zA-Z]+$/.test(sequence)) {
+            return null;
+        }
+        let currentCount = 0;
+        let currentCharacter = sequence[0];
+        let encoded = '';
+        for (let i = 0; i <= sequence.length; i++) {
+            if (currentCharacter === sequence[i]) {
+                currentCount++;
+            } else {
+                encoded += currentCharacter + currentCount;
+                currentCount = 1;
+                currentCharacter = sequence[i];
+            }
+        }
+        return encoded;
+    }
+}
